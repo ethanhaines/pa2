69a1061ee5d1b713ace5dbbf50432d8345cc3ac0
ethanhaines <122860615+ethanhaines@users.noreply.github.com>
2026-03-05 17:12:44 -0500

    base files and FIFO implemented

---
+++ src/cache.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+class Cache:
+    def __init__(self, type: str, filename: str): # k >= 1
+        self.type = type
+        self.capacity = 0
+        self.r_size = 0
+        self.__memory = []
+        self.__filename = filename
+        self.__miss = 0
+    
+    
+
+    def run(self):
+        if self.type == "FIFO":
+            return self.__FIFO()
+        elif self.type == "LRU":
+            return self.__LRU()
+        elif self.type == "OPTFF":
+            return self.__OPTFF()
+        else:
+            raise ValueError("Invalid cache type")
+    
+    def __FIFO(self):
+        with open(self.__filename, 'r') as f:
+            first_line = f.readline().strip()
+            self.capacity = int(first_line.split()[0])
+            self.r_size = int(first_line.split()[1])
+            for line in f:
+                line = line.strip()
+                if line:
+                    vals = line.split()
+                    for val in vals:
+                        if val in self.__memory:
+                            continue
+
+                        self.__miss += 1
+                        if len(self.__memory) >= self.capacity:
+                            self.__memory.pop(0)
+                        self.__memory.append(val)
+        return self.__miss
+    
+    def __LRU(self):
+        pass
+    
+    def __OPTFF(self):
+        pass
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
